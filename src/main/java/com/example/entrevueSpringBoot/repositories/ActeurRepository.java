4b40e235ea9940117c517c6ce8a0f82397ce72ae
edward cadet <e_cadet22@hotmail.com>
2021-05-07 00:32:53 -0400

    ActeurRepository removed, it was not needed

--- src/main/java/com/example/entrevueSpringBoot/repositories/ActeurRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.entrevueSpringBoot.repositories;
-
-import com.example.entrevueSpringBoot.entities.ActeurEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActeurRepository extends JpaRepository<ActeurEntity, Long> {
-}
